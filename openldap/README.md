28f25a329c16562fe4a439d45fd7e09327ab82a3
peng49 <1219955253@qq.com>
2022-02-24 18:20:08 +0800

    add openldap

---
+++ openldap/README.md
@@ -0,0 +1,3 @@
+[Install LDAP Server in Centos Step by Step](https://www.unixmen.com/install-ldap-server-in-centos-step-by-step/)
+
+[Install and Configure OpenLDAP Server on CentOS 8](https://computingforgeeks.com/install-configure-openldap-server-centos/)
---
+++ openldap/setup.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+sudo curl -L -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+#https://www.jianshu.com/p/2206cb265247
+sudo sed -ri 's/cloud.aliyuncs/aliyun/g' /etc/yum.repos.d/CentOS-Base.repo
+sudo sed -ri 's/aliyuncs.com/aliyun.com/g' /etc/yum.repos.d/CentOS-Base.repo
+
+sudo yum clean all && sudo yum makecache
+
+# 安装openldap
+sudo yum -y install openldap*
+
+
+
+
+
+
+
+
