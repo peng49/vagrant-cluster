2936f316aa655d297cc446da22ebb66709a5297b
peng49 <1219955253@qq.com>
2022-02-28 18:35:32 +0800

    update setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -5,9 +5,25 @@ sudo curl -L -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo
 sudo sed -ri 's/cloud.aliyuncs/aliyun/g' /etc/yum.repos.d/CentOS-Base.repo
 sudo sed -ri 's/aliyuncs.com/aliyun.com/g' /etc/yum.repos.d/CentOS-Base.repo
 
+sudo yum install -y epel-release
 sudo yum clean all && sudo yum makecache
 
-SCRIPT=/vagrant/${HOSTNAME}.sh
-if [ -f "${SCRIPT}" ]; then
-  sudo sh ${SCRIPT}
+
+if [ ${HOSTNAME} == 'jenkins' ];
+then
+  # install jenkins
+  # https://www.jenkins.io/doc/book/installing/linux/#red-hat-centos
+  sudo curl -L -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+  sudo yum -y upgrade
+  sudo yum install -y java-11-openjdk
+  sudo yum install -y jenkins
+  sudo systemctl daemon-reload
+
+  sudo systemctl start jenkins
+  #sudo systemctl enable jenkins
+  sudo /sbin/chkconfig jenkins on
+
+  #jenkins 初始密码
+  echo -n "jenkins init password: " && sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 fi
