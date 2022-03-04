46469b2561eee3a3c48a6f368b39ac180d5bf8a4
peng49 <1219955253@qq.com>
2022-03-04 15:24:29 +0800

    docker 搭建seafile 网盘

---
+++ seafile/BACKUP.md
@@ -0,0 +1,3 @@
+[Linux下文件实时自动同步备份](https://www.jianshu.com/p/fc2f3ec661c0)
+
+
---
+++ seafile/setup.sh
@@ -0,0 +1,49 @@
+#/bin/sh
+sudo curl -L -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+#https://www.jianshu.com/p/2206cb265247
+sudo sed -ri 's/cloud.aliyuncs/aliyun/g' /etc/yum.repos.d/CentOS-Base.repo
+sudo sed -ri 's/aliyuncs.com/aliyun.com/g' /etc/yum.repos.d/CentOS-Base.repo
+
+sudo yum clean all && sudo yum makecache
+
+# ssh允许密码登录
+sudo sed -ri 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# 允许root用户ssh登录
+sudo sed -ri 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+# 安装 docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo docker-compose -f /vagrant/docker-compose.yml up -d
+
+
+# 添加ldap认证配置
+sudo bash -c 'cat <<EOF >> /opt/seafile-data/seafile/conf/ccnet.conf
+
+[LDAP]
+HOST = ldap://192.165.33.41/
+BASE = dc=fly-develop,dc=com
+USER_DN = cn=ldapadm,dc=fly-develop,dc=com
+PASSWORD = ldap@admin
+LOGIN_ATTR = cn
+EOF'
