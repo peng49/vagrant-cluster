607b0f4e047311bf0e173f1de04758727fbdbe9b
peng49 <1219955253@qq.com>
2022-04-10 19:19:40 +0800

    init crawlab

---
+++ crawlab/setup.sh
@@ -0,0 +1,53 @@
+#! /bin/sh
+# 设置时区
+sudo timedatectl set-timezone Asia/Shanghai
+
+sudo curl -L -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+#https://www.jianshu.com/p/2206cb265247
+sudo sed -ri 's/cloud.aliyuncs/aliyun/g' /etc/yum.repos.d/CentOS-Base.repo
+sudo sed -ri 's/aliyuncs.com/aliyun.com/g' /etc/yum.repos.d/CentOS-Base.repo
+
+# ssh允许密码登录
+sudo sed -ri 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# 允许root用户ssh登录
+sudo sed -ri 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+# start docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# 设置daemon.json文件
+sudo touch /etc/docker/daemon.json
+sudo bash -c 'cat <<EOF > /etc/docker/daemon.json
+{
+        "registry-mirrors": [
+                "https://ustc-edu-cn.mirror.aliyuncs.com",
+                "https://xx4bwyg2.mirror.aliyuncs.com",
+                "http://hub-mirror.c.163.com"
+        ]
+}
+EOF'
+sudo systemctl restart docker
+
+# 安装 docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# 启动 mongodb
+# 启动 redis
+# 启动 master
+# 启动 worker
+
+
+
