f172ab15bb0dc96215744ba030b2ac480f3d38bb
peng49 <1219955253@qq.com>
2022-04-14 22:19:35 +0800

    init tidb

--- tidb/README.md
+++ tidb/README.md
@@ -1,3 +1,5 @@
 [TiDB 概述和整体架构](https://zhuanlan.zhihu.com/p/409413038)
 
 [浅析NewSQL数据库——TiDB](https://juejin.cn/post/6844903699437846536)
+
+[TiDB 数据库快速上手指南](https://docs.pingcap.com/zh/tidb/stable/quick-start-with-tidb)
---
+++ tidb/setup.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+# 设置时区
+sudo timedatectl set-timezone Asia/Shanghai
+
+sudo curl -L -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+#https://www.jianshu.com/p/2206cb265247
+sudo sed -ri 's/cloud.aliyuncs/aliyun/g' /etc/yum.repos.d/CentOS-Base.repo
+sudo sed -ri 's/aliyuncs.com/aliyun.com/g' /etc/yum.repos.d/CentOS-Base.repo
+
+sudo yum clean all && sudo yum makecache
+
+# ssh允许密码登录
+sudo sed -ri 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# 允许root用户ssh登录
+sudo sed -ri 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+# install tidb
+curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh
+source .bash_profile
+nohup tiup playground --host 0.0.0.0 &
