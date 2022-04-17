8a2b84e937d7f36cc749d0ae37b42c9bb51e162c
peng49 <1219955253@qq.com>
2022-04-17 20:30:14 +0800

    init zookeeper

--- zookeeper/README.md
+++ zookeeper/README.md
@@ -1,1 +1,65 @@
 [Zookeeper到底是干嘛的](https://www.cnblogs.com/ultranms/p/9585191.html) <br/>
+
+zoo.cfg
+```
+tickTime=2000 通信心跳时间，毫秒
+
+initLimit=10 leader follower 初始通信时限
+
+syncLimit=5 leader follower 同步通信时间
+    leader和follower之间通信时间如果超过 syncLimit * tickTime,leader认为follower已死，从服务器列表中删除follower
+
+dateDir=/opt/data/zookeeper  保存zookeeper中的数据
+
+clientPort=2181 客户端连接端口
+```
+
+zookeeper选举机制
+第一次启动
+非第一次启动
+
+
+节点信息
+```shell
+# 临时节点
+create -e /node01 'node01 con' 
+# 永久节点
+create /node02 'node02 con'
+#带序号
+create -s /node03 'node03 con'
+
+#不带序号
+create /node03 'node03 con'
+```
+
+监听器原理
+
+命令行注册监听器
+```shell
+# 注册一次监听，只能监听到一次变化
+get -w /node01
+```
+
+```shell
+# 删除节点命令 
+delete
+
+# 删除指定节点和指定节点的所有子节点
+deleteall 
+```
+
+
+写数据原理
+
+连接leader写数据
+
+
+谅解follower写数据
+
+zookeeper分布式锁原理
+
+1. 接受到请求后，在 /locks 下创建一个临时顺序节点【加锁】
+2. 客户端判断自己是不是 /locks 下最小的序号节点
+3. 如果是，执行任务，删除节点【释放锁】，如果不是对前一个节点进行监听
+
+
---
+++ zookeeper/setup.sh
@@ -0,0 +1,67 @@
+#! /bin/sh
+# 设置时区
+sudo timedatectl set-timezone Asia/Shanghai
+
+sudo curl -L -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+
+#https://www.jianshu.com/p/2206cb265247
+sudo sed -ri 's/cloud.aliyuncs/aliyun/g' /etc/yum.repos.d/CentOS-Base.repo
+sudo sed -ri 's/aliyuncs.com/aliyun.com/g' /etc/yum.repos.d/CentOS-Base.repo
+
+sudo yum clean all && sudo yum makecache
+
+# ssh允许密码登录
+sudo sed -ri 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+# 允许root用户ssh登录
+sudo sed -ri 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+# install java, zookeeper依赖java
+sudo yum install -y java-1.8.0-openjdk
+
+# install zookeeper
+curl -L https://dlcdn.apache.org/zookeeper/zookeeper-3.7.0/apache-zookeeper-3.7.0-bin.tar.gz -o apache-zookeeper-3.7.0-bin.tar.gz
+sudo tar -zxvf apache-zookeeper-3.7.0-bin.tar.gz -C /usr/local && sudo mv /usr/local/apache-zookeeper-3.7.0-bin /usr/local/zookeeper
+sudo cp /usr/local/zookeeper/conf/zoo_sample.cfg /usr/local/zookeeper/conf/zoo.cfg
+
+# 设置zookeeper数据存储路径
+sudo mkdir -p /opt/data/zookeeper
+sudo sed -ie 's/dataDir=\/tmp\/zookeeper/dataDir=\/opt\/data\/zookeeper/' /usr/local/zookeeper/conf/zoo.cfg
+
+# 设置集群id
+sudo touch /opt/data/zookeeper/myid
+
+# shellcheck disable=SC2039
+id=$(echo "${HOSTNAME}" | sed -e 's/zk0//g')
+sudo bash -c "echo ${id} > /opt/data/zookeeper/myid"
+
+# zoo.cfg添加集群配置
+sudo bash -c 'cat <<EOF >> /usr/local/zookeeper/conf/zoo.cfg
+
+server.1=192.165.34.21:2888:3888
+server.2=192.165.34.22:2888:3888
+server.3=192.165.34.23:2888:3888
+EOF'
+
+
+# 启动
+# sudo /usr/local/zookeeper/bin/zkServer.sh start
+
+# https://zhuanlan.zhihu.com/p/130265265
+# zookeeper systemd 托管
+sudo bash -c 'cat <<EOF > /usr/lib/systemd/system/zkServer.service
+[Unit]
+Description=Zookeeper server manager
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/usr/local/zookeeper/bin/zkServer.sh start
+ExecStop=/usr/local/zookeeper/bin/zkServer.sh stop
+ExecReload=/usr/local/zookeeper/bin/zkServer.sh restart
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+sudo systemctl start zkServer
