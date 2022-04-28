192e15f451d360ed31a7350669acb0da7344d86c
peng49 <1219955253@qq.com>
2022-04-28 14:50:04 +0800

    格式化

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -18,7 +18,7 @@ sudo yum install -y vim telnet bind-utils wget
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
 
-if [ ! $(getent group docker) ];
+if [ ! "$(getent group docker)" ];
 then 
     sudo groupadd docker;
 else
